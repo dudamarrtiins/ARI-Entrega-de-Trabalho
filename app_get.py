3e5122740082d3915f73a0f38cd799cdce380bac
Maria Eduarda <dudamarrtiins@gmail.com>
2026-03-18 11:32:09 -0300

    trabalhos de ari

---
+++ app_get.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Servidor(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+         self.send_response(200)
+         self.end_headers()
+         self.wfile.write(b"Servidor funcionando")
+        
+HTTPServer(("10.87.38.22", 8000), Servidor).serve_forever()
